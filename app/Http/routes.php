6ab500782773e049d40d10fb45d82cb8c92dd4ef
Ajay <ajay@qburst.com>
2015-04-23 17:24:57 +0530

    basic routing and helloworld

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('hello', 'HelloWorldController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
